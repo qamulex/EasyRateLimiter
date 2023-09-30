2037b0e6572a367fe0d6ec303663bcc7d99118ff
qamulex <qamulex@gmail.com>
2023-09-30 21:56:34 +0500

    tests renamed

--- src/test/java/me/qamulex/erl/MultiChannelTest.java
+++ src/test/java/me/qamulex/erl/MultiChannelTest.java
@@ -17,7 +17,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class MultiChannelRateLimiterTest {
+public class MultiChannelTest {
 
     final String BOB = "bob";
     final String ALICE = "alice";
--- src/test/java/me/qamulex/erl/SingleChannelTest.java
+++ src/test/java/me/qamulex/erl/SingleChannelTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class SingleChannelRateLimiterTest {
+public class SingleChannelTest {
 
     SingleChannelRateLimiter simpleLimiter = SingleChannelRateLimiter
             .builder()
