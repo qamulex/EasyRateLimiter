8a82e585f1f9394d22351a15b0300524c7f8db15
qamulex <qamulex@gmail.com>
2025-01-06 22:51:54 +0500

    RateLimiterThreadSafetyEnforcementWrapper

---
+++ src/main/java/me/qamulex/easyratelimiter/RateLimiterThreadSafetyEnforcementWrapper.java
@@ -0,0 +1,56 @@
+/*
+ * Created on Jan 06, 2025
+ *
+ * Copyright (c) qamulex
+ */
+package me.qamulex.easyratelimiter;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import lombok.NonNull;
+
+public class RateLimiterThreadSafetyEnforcementWrapper implements RateLimiter {
+
+    private final RateLimiter wrappedRateLimiter;
+
+    public RateLimiterThreadSafetyEnforcementWrapper(@NonNull RateLimiter wrappedRateLimiter) {
+        this.wrappedRateLimiter = wrappedRateLimiter;
+    }
+
+    @Override
+    public synchronized long getTimeUntilNextRequest() {
+        return wrappedRateLimiter.getTimeUntilNextRequest();
+    }
+
+    @Override
+    public synchronized boolean isRequestAllowed() {
+        return wrappedRateLimiter.isRequestAllowed();
+    }
+
+    @Override
+    public synchronized boolean tryRequest() {
+        return wrappedRateLimiter.tryRequest();
+    }
+
+    @Override
+    public void blockUntilRequestAllowed() throws InterruptedException {
+        wrappedRateLimiter.blockUntilRequestAllowed();
+    }
+
+    @Override
+    public boolean blockUntilRequestAllowed(long timeout, TimeUnit unit) throws InterruptedException {
+        return wrappedRateLimiter.blockUntilRequestAllowed(timeout, unit);
+    }
+
+    @Override
+    public boolean blockUntilRequestAllowed(Duration duration) throws InterruptedException {
+        return wrappedRateLimiter.blockUntilRequestAllowed(duration);
+    }
+
+    @Override
+    public synchronized void reset() {
+        wrappedRateLimiter.reset();
+    }
+
+}
