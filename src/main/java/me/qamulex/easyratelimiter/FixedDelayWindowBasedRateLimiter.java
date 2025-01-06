f6e08745d6e5799bf3565bbfef3d938b3e133aea
qamulex <qamulex@gmail.com>
2025-01-06 22:42:51 +0500

    FixedDelayWindowBasedRateLimiter

---
+++ src/main/java/me/qamulex/easyratelimiter/FixedDelayWindowBasedRateLimiter.java
@@ -0,0 +1,60 @@
+/*
+ * Created on Jan 06, 2025
+ *
+ * Copyright (c) qamulex
+ */
+package me.qamulex.easyratelimiter;
+
+import lombok.NonNull;
+
+public class FixedDelayWindowBasedRateLimiter extends ClockDependentRateLimiter {
+
+    private final FixedDelayRateLimiter  fixedDelayRateLimiter;
+    private final WindowBasedRateLimiter windowBasedRateLimiter;
+
+    public FixedDelayWindowBasedRateLimiter(
+            @NonNull FixedDelayRateLimiter fixedDelayRateLimiter,
+            @NonNull WindowBasedRateLimiter windowBasedRateLimiter
+    ) {
+        this.fixedDelayRateLimiter = fixedDelayRateLimiter;
+        this.windowBasedRateLimiter = windowBasedRateLimiter;
+    }
+
+    @Override
+    protected long getTimeUntilNextRequest(long timeMillis) {
+        return Math.max(
+                fixedDelayRateLimiter.getTimeUntilNextRequest(timeMillis),
+                windowBasedRateLimiter.getTimeUntilNextRequest(timeMillis)
+        );
+    }
+
+    @Override
+    protected boolean isRequestAllowed(long timeMillis) {
+        return fixedDelayRateLimiter.isRequestAllowed(timeMillis)
+                && windowBasedRateLimiter.isRequestAllowed(timeMillis);
+    }
+
+    @Override
+    public synchronized boolean tryRequest() {
+        return isRequestAllowed()
+                && fixedDelayRateLimiter.tryRequest()
+                && windowBasedRateLimiter.tryRequest();
+    }
+
+    @Override
+    public void reset() {
+        fixedDelayRateLimiter.reset();
+        windowBasedRateLimiter.reset();
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "FixedDelayWindowBasedRateLimiter [%s, %s]",
+     
+                fixedDelayRateLimiter,
+                windowBasedRateLimiter
+        );
+    }
+
+}
